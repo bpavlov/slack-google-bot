77337513b18ab863690b36042c718c4c59b0d2e7
Boris Pavlov <bnpavlov@gmail.com>
2016-09-17 15:24:14 +0300

    Update about.rb

--- slack-google-bot/about.rb
+++ slack-google-bot/about.rb
@@ -1,7 +1,5 @@
 module SlackGoogleBot
   ABOUT = <<-ABOUT
-    #{SlackGoogleBot::VERSION}
-    https://github.com/dblock/slack-google-bot
-    https://twitter.com/dblockdotorg
+    Hello, I am Google Slack Bot #{SlackGoogleBot::VERSION}!
   ABOUT
 end
